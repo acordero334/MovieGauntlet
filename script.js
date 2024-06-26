d78d96d572e11f344dcc08171193a1234004d9e4
acordero334 <acordero334@gmail.com>
2024-06-26 15:31:09 -0500

    Big Update
    Continuing to develop the program. It's ugly but i was able to get it to search for movies and display certain information when clicking a result.

--- script.js
+++ script.js
@@ -2,15 +2,19 @@
 
 const movieSearchBox = document.getElementById('movie-search-box');
 const searchList = document.getElementById('search-list');
+const resultGrid = document.getElementById('result-grid');
 
 //load movies from API
 async function loadMovies(searchTerm){
-    const URL = 'https://omdbapi.com/?s=${searchTerm}&page=1&apikey=cf0a3c31';
+    const URL = `https://omdbapi.com/?s=${searchTerm}&page=1&apikey=cf0a3c31`;
     const res = await fetch(`${URL}`);
     const data = await res.json();
 
 
-    if(data.Response == "True") displayMovieList(data.Search);
+    if(data.Response == "True"){
+        displayMovieList(data.Search)
+        }
+       ;
 }
 
 function findMovies(){
@@ -18,17 +22,23 @@ function findMovies(){
     if (searchTerm.length > 0){
         searchList.classList.remove('hide-search-list');
         loadMovies(searchTerm);
+        
     } else {
         searchList.classList.add('hide-search-list');
     }
+}
 
-function displayMovieList(movies){
+function displayMovieList(movies){;
     searchList.innerHTML = "";
     for (let idx = 0; idx < movies.length; idx ++){
+
         let movieListItem = document.createElement('div'); //creating a div within the HTML
         movieListItem.dataset.id = movies[idx].imdbID;
         movieListItem.classList.add('search-list-item');
+
+
         if (movies[idx].Poster != "N/A"){
+
             moviePoster = movies[idx].Poster; 
             
         }else {
@@ -40,16 +50,57 @@ function displayMovieList(movies){
             <div class = "search-item-thumbnail">
                 <img src = "${moviePoster}">
             </div>
-            
             <div class = "search-item-info">
                 <h3>${movies[idx].Title}</h3>
                 <p>${movies[idx].Year}</p>
             </div>
-        `}
+        `;
+    searchList.appendChild(movieListItem);
+    }
+        
+        
         loadMovieDetails();
 }
 
 function loadMovieDetails(){
     
+    const searchMovieList = searchList.querySelectorAll('.search-list-item');
+    searchMovieList.forEach(movie => {
+        movie.addEventListener('click', async () => {
+            //console.log(movie.dataset.id);
+            searchList.classList.add('hide-search-list');
+            movieSearchBox.value = "";
+            const result = await fetch(`http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=cf0a3c31`);
+            const movieDetails = await result.json();
+            console.log(movieDetails);
+            displayMovieDetails(movieDetails);
+
+            //adding clicked movie to an array? we'll see how this goes
+        });
+    });
+
 }
+
+function displayMovieDetails(details){
+    resultGrid.innerHTML = `
+    <div class = "movie-poster">
+    <img src = "${(details.Poster != "N/A") ? details.Poster : "image-not-found.png"}" alt = "Movie Poster">
+    </div>
+    <div class="movie-info">
+    <h3 class= "movie-title">${details.Title}</h3>
+    <ul class= "movie-misc-info">
+    <li class= "movie-year"> ${details.Year}</li>
+    <li class= "movie-rating"> ${details.Rated}</li> 
+    </ul>
+    <p class = "movie-plot">${details.Plot}</p>
+    </div>
+   `;
 }
+
+window.addEventListener('click', (event) =>{
+    if(event.target.className != "form-control"){
+        searchList.classList.add('hide-search-list');
+    }
+});
+
+
